2f6210af2a5fc302c0fd47495bb784a583449ee6
craju <craju@exafluence.com>
2025-05-21 17:53:52 +0530

    added sample react based application

---
+++ karedemo/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <header className="App-header">
+        <div style={{display: "flex"}}>
+          Sample Project for Kare To Test the CICD
+        </div>
+        <div>Attempt: 01</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
